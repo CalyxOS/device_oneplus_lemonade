cf0bc40afff359fbc4374d6d098755db9bb99ab2
Chirayu Desai <chirayudesai1@gmail.com>
2021-09-10 02:51:41 +0530

    Initial calyx makefiles
    Change-Id: Ia1c2050f8216ce75374d4b55c3bc94e9d3d97b4e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_lemonade.mk \
     $(LOCAL_DIR)/lineage_lemonade.mk
 
 COMMON_LUNCH_CHOICES := \
+    calyx_lemonade-user \
+    calyx_lemonade-userdebug \
+    calyx_lemonade-eng \
     lineage_lemonade-user \
     lineage_lemonade-userdebug \
     lineage_lemonade-eng
