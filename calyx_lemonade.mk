6404ebf5d97f1a89be754ba7f970e8dadad2b92b
Michael Bestas <mkbestas@gmail.com>
2022-06-28 03:17:15 +0300

    Update calyx_lemonade.mk from lineage_lemonade.mk
    Change-Id: I41e157d118f7098dc1f85786fde60ebf7d2b7637

--- calyx_lemonade.mk
+++ calyx_lemonade.mk
@@ -1,17 +1,7 @@
 #
 # Copyright (C) 2021-2022 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit some common stuff.
@@ -28,14 +18,14 @@ PRODUCT_NAME := calyx_lemonade
 PRODUCT_DEVICE := lemonade
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2113
+PRODUCT_MODEL := LE2115
 
-PRODUCT_SYSTEM_NAME := OnePlus9_EEA
+PRODUCT_SYSTEM_NAME := OnePlus9
 PRODUCT_SYSTEM_DEVICE := OnePlus9
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus9-user 12 SKQ1.210216.001 R.202204081955 release-keys" \
     TARGET_DEVICE=OnePlus9 \
-    TARGET_PRODUCT=OnePlus9_EEA \
-    PRIVATE_BUILD_DESC="OnePlus9-user 11   release-keys"
+    TARGET_PRODUCT=OnePlus9
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9_EEA/OnePlus9:11/RKQ1.201105.002/2111112053:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:12/SKQ1.210216.001/R.202204081955:user/release-keys
